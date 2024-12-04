4308097bab53339bcb5f90fa7fa806452653eb0d
Kaiquekaan <kaiqueandrade188@gmai.com>
2024-12-04 17:48:04 -0300

    Nova atualização backend 10

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,23 +8,6 @@ from django.urls import path
 from django.core.management import call_command
 from django.conf import settings
 
-def run_migrations():
-    try:
-        print("Criando migrações...")
-        call_command('makemigrations')  # Cria as migrações com base nos modelos alterados
-        
-        print("Aplicando migrações...")
-        call_command('migrate')  # Aplica as migrações ao banco de dados
-        
-        print("Migrações criadas e aplicadas com sucesso!")
-    except Exception as e:
-        print(f"Erro ao criar ou aplicar migrações: {e}")
-
-# Certifique-se de que o Django foi configurado corretamente
-if settings.configured:
-    run_migrations()
-else:
-    print("Erro: O Django não foi configurado corretamente.")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
